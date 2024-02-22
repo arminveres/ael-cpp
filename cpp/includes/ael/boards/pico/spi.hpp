be59d5fd5d61190f17a972ac62f66aef52a5297f
Armin Veres <armin.veres@hotmail.com>
2024-02-22 17:11:22 +0100

    feat: add spi and testcase

--- cpp/includes/ael/boards/pico/spi.hpp
+++ cpp/includes/ael/boards/pico/spi.hpp
@@ -1,13 +1,27 @@
 #ifndef __AEL_BOARDS_PICO_SPI_HPP
 #define __AEL_BOARDS_PICO_SPI_HPP
+#include <hardware/gpio.h>
+#include <hardware/spi.h>
+
 #include "ael/types.hpp"
 
 namespace ael::boards::pico {
 
-template <u8 MOSI>
+enum class eInstSPI { SPI_0, SPI_1 };
+
+template <eInstSPI SPI_INSTANCE, u8 CS, u8 SCLK, u8 MOSI, u8 MISO, u32 SPI_SPEED>
 class SPI {
    public:
-    SPI() {}
+    SPI() {
+        // Pico Initialization of SPI
+        if constexpr (SPI_INSTANCE == eInstSPI::SPI_0)
+            spi_init(spi0, SPI_SPEED);
+        else
+            spi_init(spi1, SPI_SPEED);
+
+        gpio_set_function(SCLK, GPIO_FUNC_SPI);
+        gpio_set_function(MOSI, GPIO_FUNC_SPI);
+    }
     SPI(SPI &&) = default;
     SPI(const SPI &) = default;
     SPI &operator=(SPI &&) = default;
--- test/test1.cpp
+++ test/test1.cpp
@@ -1,6 +1,19 @@
 #include "ael/boards/pico/spi.hpp"
 
-int main(int argc, char *argv[]) {
-    ael::boards::pico::SPI<10u> inst;
+using namespace ael::boards::pico;
+
+int main() {
+    // RX Pin
+    static constexpr auto MISO_PIN = 8;
+    static constexpr auto CS_PIN = 9;
+    static constexpr auto CLK_PIN = 10;
+    // TX Pin
+    static constexpr auto MOSI_PIN = 11;
+
+    // static constexpr auto RST_PIN = 12;
+    // static constexpr auto BL_PIN = 13;
+
+    SPI<eInstSPI::SPI_0, CS_PIN, CLK_PIN, MOSI_PIN, MISO_PIN, 10'000 * 1000> inst;
+
     return 0;
 }
