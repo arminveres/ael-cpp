2287ef5af0746c2383ca2c615cd27c0e6cf3361d
Armin Veres <armin.veres@hotmail.com>
2024-02-22 14:57:06 +0100

    feat: move array to sub include and att type definitions

--- cpp/includes/ael/array.hpp
+++ cpp/includes/ael/array.hpp
@@ -1,5 +1,5 @@
-#ifndef AEL_ARRAY_H
-#define AEL_ARRAY_H
+#ifndef AEL_ARRAY_HPP
+#define AEL_ARRAY_HPP
 
 #include <cstddef>
 #include <optional>
@@ -9,10 +9,12 @@ namespace ael {
 /**
  * @brief This class is meant as a replacement for the std::array, since that is made constexpr and
  * I need to be able to interface with C code.
+ *
+ * @note Very much WIP!
  */
 template <typename T, size_t SIZE>
 class array {
-    T m_array[SIZE];
+    T m_array[SIZE] = {};
     size_t m_len = SIZE;
 
    public:
@@ -25,6 +27,7 @@ class array {
 
     /**
      * @brief Get the element at index i, wrappen into std::optional
+     *
      * @returns std::nullopt if no available value, value T otherwise
      */
     std::optional<T> get(const size_t& i) {
@@ -35,4 +38,4 @@ class array {
 
 }  // namespace ael
 
-#endif  // !AEL_ARRAY_H
+#endif  // !AEL_ARRAY_HPP
---
+++ cpp/includes/ael/types.hpp
@@ -0,0 +1,24 @@
+#ifndef AEL_TYPES_H_
+#define AEL_TYPES_H_
+
+#include <cmath>
+#include <cstdint>
+
+namespace ael {
+
+// Unsigned
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+
+// Signed
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+
+// floating point numbers
+using f32 = std::float_t;
+using f64 = std::double_t;
+
+}  // namespace ael
+#endif  // ! AEL_TYPES_H_
