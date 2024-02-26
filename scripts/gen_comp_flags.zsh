d8eae88b53b8d39bcc0ebf13f4d18db252287800
Armin Veres <armin.veres@hotmail.com>
2024-02-26 19:27:40 +0100

    build: add zsh version for generating compile flags
    much cleaner and correctly parsed include directories even with whitespaces

---
+++ scripts/gen_comp_flags.zsh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+set -e
+
+FILE=cpp/compile_flags.txt
+pref="-I"
+
+cat <<EOF >$FILE
+/opt/gcc-arm-none-eabi/bin/arm-none-eabi-g++
+-std=c++20,
+EOF
+
+pushd cpp
+files="$(fd 'include*' -t d ..)"
+popd
+
+# Split the string on newlines and loop through each line
+for line in ${(f)files}; do
+    # Prefix each line with '-I' and print
+    echo "$pref$line" >> $FILE
+done
