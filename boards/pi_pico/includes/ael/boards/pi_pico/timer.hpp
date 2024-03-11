a4e50d1d02cdb07780455e1508958b8a2ce1369d
Armin Veres <armin.veres@hotmail.com>
2024-03-11 21:31:33 +0100

    feat(timer): add triple tuple methods

--- boards/pi_pico/includes/ael/boards/pi_pico/timer.hpp
+++ boards/pi_pico/includes/ael/boards/pi_pico/timer.hpp
@@ -2,10 +2,15 @@
 #define __AEL_BOARDS_PI_PICO_TIMER_HPP
 #include <hardware/timer.h>
 
+#include <tuple>
+
 #include "ael/types.hpp"
 
 using namespace ael::types;
+
 namespace ael::boards::pi_pico::timer {
+
+using TimeStamp = std::tuple<u64, u64, u64>;
 
 enum class eTimeType { eMicro, eMilli, eSec };
 
@@ -23,21 +28,50 @@ struct Timer {
         else if constexpr (type == eTimeType::eMilli)
             return diff / 1000;
         else if constexpr (type == eTimeType::eSec)
-            return diff / 1000 * 1000;
+            return diff / (1000 * 1000);
     };
 
     [[nodiscard]] auto get_now() -> u64 {
         if constexpr (type == eTimeType::eMicro)
-            return start_time;
+            return time_us_64();
         else if constexpr (type == eTimeType::eMilli)
-            return start_time / 1000;
+            return time_us_64() / 1000;
         else if constexpr (type == eTimeType::eSec)
-            return start_time / (1000 * 1000);
+            return time_us_64() / (1000 * 1000);
     }
+
+    /**
+     * @brief Returns the current time in Hours, Minutes and Seconds as a tuple
+     */
+    [[nodiscard]] auto get_now_triple() -> TimeStamp { return convert_to_tuple(get_now()); }
+
+    /**
+     * @brief Stops the clock and returns the current time in Hours, Minutes and Seconds as a tuple
+     */
+    [[nodiscard]] auto stop_with_triple() -> TimeStamp { return convert_to_tuple(stop()); }
 
    private:
     /// start time in microseconds
     u64 start_time = 0;
+
+    /**
+     * @brief Convert time to timestep in Hours, Minutes and seconds
+     */
+    auto convert_to_tuple(const u64 now) -> TimeStamp {
+        auto now_s = 0;
+        if constexpr (type == eTimeType::eMicro) {
+            now_s = now / (1000 * 1000);
+        } else if constexpr (type == eTimeType::eMilli) {
+            now_s = now / 1000;
+        } else if constexpr (type == eTimeType::eSec) {
+            now_s = now;
+        }
+        const auto hours = now_s / (60 * 60);
+        auto rem_s = now % (60 * 60);
+        const auto mins = rem_s / 60;
+        rem_s = rem_s % 60;
+        return {hours, mins, rem_s};
+    }
 };
 }  // namespace ael::boards::pi_pico::timer
 
