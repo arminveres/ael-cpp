4c0d8796aad9afd208a310e90f7210648ff48fb2
Armin Veres <armin.veres@hotmail.com>
2024-06-15 16:23:59 +0200

    feat: implement adxl345 accelerometer

---
+++ boards/pi_pico/extras/adxl345.cpp
@@ -0,0 +1,56 @@
+#include "ael/boards/pi_pico/extras/adxl345.hpp"
+
+#include <pico/platform.h>
+#include <pico/stdlib.h>
+
+#include <optional>
+
+#include "ael/boards/pi_pico/spi.hpp"
+#include "ael/peripherals/adxl345/registers.hpp"
+#include "ael/types.hpp"
+#include "pico/time.h"
+
+using namespace ael::boards::pi_pico::extras::adxl345;
+using namespace ael::boards::pi_pico::spi;
+using namespace ael::types;
+using namespace ael::peripherals::adxl345;
+
+ADXL345::ADXL345(SPI &p_spi, const u8 sampling_rate)
+    : m_spi(p_spi), m_sampling_rate(sampling_rate) {}
+
+ADXL345::~ADXL345() {}
+
+auto ADXL345::init() -> std::optional<eError> {
+    this->reg_set(ADXL345_REG_POWER_CTL, 0x08);
+    return std::nullopt;
+}
+
+auto ADXL345::reg_set(const u8 reg, const u8 value) const -> void { m_spi.rwrite<1>(reg, &value); }
+
+auto ADXL345::reg_update(const u8 reg, const u8 val) const -> void { m_spi.rupdate(reg, val); }
+
+[[nodiscard]] auto ADXL345::reg_read(const u8 reg) const -> u8 {
+    u8 ldata = 255;
+    const auto retval = m_spi.rread<1>(reg | READ_FLAG, &ldata);
+    if (retval == 0) {
+        // panic("PANIC: no data received...");
+        while (true) {
+            printf("no data read...\n");
+            sleep_ms(1000);
+        }
+    }
+    return ldata;
+    // if (not m_spi->rread<1>(reg, &ldata)) return std::unexpected(eError::errorGeneric);
+}
+
+[[nodiscard]] auto ADXL345::read_accel() const -> Accel {
+    u16 rawx, rawy, rawz;
+    m_spi.rread<2>(ADXL345_REG_DATAX0 | READ_FLAG | AUTO_INCREMENT_FLAG,
+                   reinterpret_cast<u8 *>(&rawx));
+    m_spi.rread<2>(ADXL345_REG_DATAY0 | READ_FLAG | AUTO_INCREMENT_FLAG,
+                   reinterpret_cast<u8 *>(&rawy));
+    m_spi.rread<2>(ADXL345_REG_DATAZ0 | READ_FLAG | AUTO_INCREMENT_FLAG,
+                   reinterpret_cast<u8 *>(&rawz));
+
+    return Accel{rawx, rawy, rawz};
+}
---
+++ boards/pi_pico/includes/ael/boards/pi_pico/extras/adxl345.hpp
@@ -0,0 +1,31 @@
+#ifndef __AEL_BOARDS_PI_PICO_EXTRAS_ADXL345_HPP
+#define __AEL_BOARDS_PI_PICO_EXTRAS_ADXL345_HPP
+#include <optional>
+
+#include "../spi.hpp"
+#include "ael/peripherals/interfaces/accelerometer.hpp"
+#include "ael/types.hpp"
+
+namespace ael::boards::pi_pico::extras::adxl345 {
+
+class ADXL345 : private peripherals::interfaces::I_Accelerometer {
+   public:
+    static constexpr types::u8 ADXL345_ID = 0xE5;
+
+    explicit ADXL345(spi::SPI& p_spi, const types::u8 sampling_rate = 0);
+    ~ADXL345();
+
+    auto init() -> std::optional<types::eError> override;
+    auto reg_set(const types::u8 reg, const types::u8 value) const -> void override;
+    auto reg_update(const types::u8 reg, const types::u8 val) const -> void override;
+    [[nodiscard]] auto reg_read(const types::u8 reg) const -> types::u8 override;
+    [[nodiscard]] auto read_accel() const -> Accel override;
+
+   private:
+    spi::SPI& m_spi;
+    types::u8 m_sampling_rate;
+};
+
+}  // namespace ael::boards::pi_pico::extras::adxl345
+
+#endif  // !__AEL_BOARDS_PI_PICO_EXTRAS_ADXL345_HPP
